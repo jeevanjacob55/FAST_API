ec5f5c536a5667ac1787aac4564bd9c4cfe516c4
jeevanjacob55 <jeevanjacob55@gmail.com>
2025-08-08 19:10:20 +0530

    polyline fetching test successfully passed.the server is running smoothely

--- rushr/app/main.py
+++ rushr/app/main.py
@@ -5,7 +5,9 @@ from app.db import create_tables
 
 app = FastAPI()
 create_tables()
-
+@app.get("/")
+def read_root():
+    return {"status": "Rushr backend is running"}
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(rides.router, prefix="/rides", tags=["Rides"])
 
---
+++ rushr/tests/test_google_maps.py
@@ -0,0 +1,21 @@
+
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+# GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
+# print(GOOGLE_MAPS_API_KEY)
+from app.utils.google_maps import fetch_route_polyline
+
+def test_polyline_fetch():
+    origin = "Kochi, IN"
+    destination = "Bangalore, IN"
+    
+    try:
+        polyline = fetch_route_polyline(origin, destination)
+        assert isinstance(polyline, str) and len(polyline) > 0
+        print("✅ Test Passed: Polyline fetched successfully.")
+    except Exception as e:
+        print(f"❌ Test Failed: {e}")
+
+test_polyline_fetch()
